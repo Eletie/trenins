a851ee9e1d43b6c4ffec6ed2bc98ac2a7c6d7fbb
Eletie <eletie040805@users.noreply.github.com>
2020-01-22 16:13:59 +0200

    V1

---
+++ tests.py
@@ -0,0 +1,25 @@
+print("atbildi uz jautājumu!  ")
+a = input("1+1=:")
+print("Tu ievadiji: " + a)
+
+if (a==2):
+  print(" viss pareizi")
+else:
+  print("nepareizi")
+
+b = input("4+3=:")
+print("Tu ievadiji: " + b)
+
+if (b==7):
+  print ("tu ģēnijs?")
+else:
+  print('es pat zinu')
+
+c = input("69+69=:")
+print("Tu ievadiji: " + c)
+
+if (c==138):
+  print("malacis matemātikas burvi!")
+else:
+    print("stulbenis...;)")
+
